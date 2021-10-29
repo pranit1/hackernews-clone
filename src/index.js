4e4e01daff3347a1360001b68e129ee905af9aa3
Pranit Shinde <pspranits10494@gmail.com>
2021-10-29 19:23:34 +0530

    connecting prisma client with graphql server

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 const {ApolloServer} = require('apollo-server');
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 const fs = require('fs');
 const path = require('path');
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack  GraphQL'
-  
-}]
 const resolvers = {
     Query: {
         info: () => `This is the API of a hackernews clone`,
-        feed: () => links,
+        feed: async (parent, args, context) => {
+            return context.prisma.link.findMany()
+        }
     },
 
     Mutation: {
-        post: (parent, args) => {
-            let idCount = links.length
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            return link
+        post: (parent, args, context, info) => {
+            const newLink = context.prisma.link.create(
+                {
+                    data: {
+                        url: args.url,
+                        description: args.description
+                    }
+                }
+            )
+            return newLink
         }
     }
 }
@@ -33,7 +32,10 @@ const server = new ApolloServer({
         path.join(__dirname, 'schema.graphql'),
         'utf8'
     ),
-    resolvers
+    resolvers,
+    context: {
+        prisma,
+      }
 })
 
 

c69c400e5800d89d18b88e21e29ad2b1fbb7e0cf
Pranit Shinde <pspranits10494@gmail.com>
2021-10-31 14:12:43 +0530

    adding authentication and voting

--- src/index.js
+++ src/index.js
@@ -3,41 +3,36 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const fs = require('fs');
 const path = require('path');
-
+const { getUserId } = require('./utils');
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutations')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Vote = require('./resolvers/Vote')
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a hackernews clone`,
-        feed: async (parent, args, context) => {
-            return context.prisma.link.findMany()
-        }
-    },
-
-    Mutation: {
-        post: (parent, args, context, info) => {
-            const newLink = context.prisma.link.create(
-                {
-                    data: {
-                        url: args.url,
-                        description: args.description
-                    }
-                }
-            )
-            return newLink
-        }
-    }
+    Vote,
+    Query,
+    Mutation,
+    User,
+    Link
 }
-
 const server = new ApolloServer({
-    typeDefs: fs.readFileSync(
-        path.join(__dirname, 'schema.graphql'),
-        'utf8'
-    ),
-    resolvers,
-    context: {
-        prisma,
-      }
-})
-
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  }
+});
 
 server
     .listen()
